99c8ca052014db090f90ab6db30ad1a00191fd23
J Alan Brogan <github@al-got-rhythm.net>
2013-12-05 22:19:45 +0000

    Extract method, better naming

--- src/parsing/parsley/hosts.py
+++ src/parsing/parsley/hosts.py
@@ -1,4 +1,4 @@
-"""A module to provide methods to parse hosts files"""
+"""A script to parse hosts files"""
 
 
 import os
@@ -11,7 +11,7 @@ import chimichurri
 
 def read_hosts_grammar():
 	"""Make a grammar for hosts files"""
-	return chimichurri.read_makeParser('hosts.parsley')
+	return chimichurri.read_grammar('hosts.parsley')
 
 
 def parse_text(input_string):
@@ -21,18 +21,26 @@ def parse_text(input_string):
 	return grammar_wrapper.hosts()
 
 
-def parse_path(hosts_file):
-	if not os.path.isfile(hosts_file):
-		raise ValueError('%r is not a file' % hosts_file)
-	input_string = file(hosts_file).read()
+def require_file(path_to_file):
+	"""If the given path is not a file then raise ValueError"""
+	if not os.path.isfile(path_to_file):
+		raise ValueError('%r is not a file' % path_to_file)
+
+
+def parse_path(path_to_hosts):
+	"""Parse the hosts file at the given path"""
+	require_file(path_to_hosts)
+	input_string = file(path_to_hosts).read()
 	return parse_text(input_string)
 
 
 def main(args):
+	"""Run the program"""
 	for arg in args:
 		parsed = parse_path(arg)
 		pprint(parsed)
 	return os.EX_OK
+
 
 if __name__ == '__main__':
 	sys.exit(main(sys.argv[1:]))
