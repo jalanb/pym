5388ad2d93eb100c40919cd71a4468f8ac3ea7a8
J Alan Brogan <github@al-got-rhythm.net>
2014-04-26 03:40:30 +0100

    restore space

--- docs/build.sh
+++ docs/build.sh
@@ -10,7 +10,7 @@ make_docs ()
 {
 	make clean > build.clean.log 2>&1
 	make html > build.html.log 2>&1
-	if [[ -n $SITE && -d $SITE]]
+	if [[ -n $SITE && -d $SITE ]]
     then rsync -a _build/html $SITE
     fi
 }
