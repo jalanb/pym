a303e189b4c2b3f97dffe19e95226411397b5a09
J Alan Brogan <github@al-got-rhythm.net>
2014-05-08 01:08:14 +0100

    move short version up to release version

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2014, jalanb (Alan Brogan)'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.1.3'
 
