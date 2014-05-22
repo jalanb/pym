fc115efb604e4a74337312d9987aaa908033455a
J Alan Brogan <github@al-got-rhythm.net>
2014-05-22 19:50:07 +0100

    increase test coverage

--- pym/render/test/test_transformers.py
+++ pym/render/test/test_transformers.py
@@ -8,27 +8,58 @@ from pym.render.transformers import PymTransformer
 from pym.render import render
 
 
-class MockTransformer(PymTransformer):
-    def visit_Assign(self, node):
-        # pylint: disable=no-self-use
-        if node.lineno == 2:
-            return None
-        return node
-
-
 class PymTransformerTest(TestCase):
 
-    def setUp(self):
-        self.ast = render.parse('i = 0\nj = i = 1')
-
     def test_transformer(self):
-        expected = self.ast
+        expected = render.parse('i = 0\nj = i = 1')
         transformer = PymTransformer()
         actual = transformer.visit(expected)
         self.assertEqual(expected, actual)
 
-    def test_removal(self):
-        transformer = MockTransformer()
-        ast = transformer.visit(self.ast)
+    def test_pass_removal(self):
+        class PassRemover(PymTransformer):
+            def visit_Pass(self, _node):
+                # pylint: disable-msg=no-self-use
+                return None
+
+        transformer = PassRemover()
+        ast = render.parse('pass')
+        ast = transformer.visit(ast)
         actual = render.render(ast)
-        self.assertNotIn('i = 1', actual)
+        self.assertEqual('', actual)
+
+    def test_string_emptying(self):
+        class StringBlanker(PymTransformer):
+            def visit_Str(self, _node):
+                # pylint: disable-msg=no-self-use
+                return 'pass'
+
+        transformer = StringBlanker()
+        ast = render.parse('"contents"')
+        ast = transformer.visit(ast)
+        actual = render.render(ast)
+        self.assertEqual('pass', actual)
+
+    def test_string_removal_from_module(self):
+        class StringBlanker(PymTransformer):
+            def visit_Expr(self, _node):
+                # pylint: disable-msg=no-self-use
+                return None
+
+        transformer = StringBlanker()
+        ast = render.parse('"contents"')
+        ast = transformer.visit(ast)
+        actual = render.render(ast)
+        self.assertEqual('', actual)
+
+    def test_string_removal_from_expression(self):
+        class StringBlanker(PymTransformer):
+            def visit_Expr(self, _node):
+                # pylint: disable-msg=no-self-use
+                return None
+
+        transformer = StringBlanker()
+        ast = render.parse('i=0\n"contents"')
+        ast = transformer.visit(ast)
+        actual = render.render(ast)
+        self.assertEqual('i = 0', actual)
