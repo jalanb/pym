2a92ae057ad704544b690439912654975930dd55
J Alan Brogan <github@al-got-rhythm.net>
2014-05-22 20:58:58 +0100

    split code for ease of inheritance

--- pym/render/transformers.py
+++ pym/render/transformers.py
@@ -27,35 +27,51 @@ class PymTransformer(ast.NodeTransformer):
     That license file is in this directory as "PYTHONLICENSE.txt"
     """
 
-    def before_old_value(self, new_values, value):
+    def before_old_ast_value(self, new_values, value):
         pass
 
     def add_new_value(self, new_values, value):
         # pylint: disable=no-self-use
         new_values.append(value)
 
+    def handle_new_value(self, new_values, new_value):
+        # pylint: disable-msg=no-self-use
+        if new_value is None:
+            return True
+        if not isinstance(new_value, ast.AST):
+            new_values.extend(new_value)
+            return True
+        return False
+
+    def handle_old_value(self, new_values, old_value):
+        if not isinstance(old_value, ast.AST):
+            self.add_new_value(new_values, old_value)
+        else:
+            self.before_old_ast_value(new_values, old_value)
+            new_value = self.visit(old_value)
+            if self.handle_new_value(new_values, new_value):
+                return
+            self.add_new_value(new_values, new_value)
+
+    def handle_list(self, old_value):
+        new_values = []
+        for old_value in old_value:
+            self.handle_old_value(new_values, old_value)
+        return new_values
+
+    def handle_item(self, field, node, old_value):
+        new_node = self.visit(old_value)
+        if new_node is None:
+            delattr(node, field)
+        else:
+            setattr(node, field, new_node)
+
     def generic_visit(self, node):
         for field, old_value in ast.iter_fields(node):
-            old_value = getattr(node, field, None)
             if isinstance(old_value, list):
-                new_values = []
-                for value in old_value:
-                    if isinstance(value, ast.AST):
-                        self.before_old_value(new_values, value)
-                        value = self.visit(value)
-                        if value is None:
-                            continue
-                        if not isinstance(value, ast.AST):
-                            new_values.extend(value)
-                            continue
-                    self.add_new_value(new_values, value)
-                old_value[:] = new_values
+                old_value[:] = self.handle_list(old_value)
             elif isinstance(old_value, ast.AST):
-                new_node = self.visit(old_value)
-                if new_node is None:
-                    delattr(node, field)
-                else:
-                    setattr(node, field, new_node)
+                self.handle_item(field, node, old_value)
         return node
 
 
@@ -94,7 +110,7 @@ class Commenter(PymTransformer):
         except IndexError:
             self.comment = NoComment((-1, -1, ''))
 
-    def before_old_value(self, new_values, value):
+    def before_old_ast_value(self, new_values, value):
         while self.comment.is_line_before(value):
             new_values.append(self.comment)
             self.next_comment()
