7e87aefef6df163dfdf8018e8985ed38f72aee5d
J Alan Brogan <github@al-got-rhythm.net>
2014-11-02 00:45:26 +0000

    shorten method call

--- pym/edit/main.py
+++ pym/edit/main.py
@@ -23,17 +23,13 @@ def start_debugging():
 def parse_args():
     """Parse out command line arguments"""
     parser = argparse.ArgumentParser(description=__doc__.splitlines()[0])
-    parser.add_argument('items', nargs='*',
-                        help='items to be editted')
-    parser.add_argument('-c', '--command',
-                        help='act like vim for a string of hjkl commands',
-                        action='store',
-                        default=list(),
-                        type=str)
-    parser.add_argument('-q', '--quiet', action='store_true',
-                        help='Show less output')
-    parser.add_argument('-U', '--Use_debugger', action='store_true',
-                        help='Run the script with pdb (or pudb if available)')
+    pa = parser.add_argument
+    pa('items', nargs='*', help='items to be editted')
+    pa('-c', '--command', action='store', default=list(), type=str,
+       help='act like vim for a string of hjkl commands',)
+    pa('-q', '--quiet', action='store_true', help='Show less output')
+    pa('-U', '--Use_debugger', action='store_true',
+       help='Run the script with pdb (or pudb if available)')
     args = parser.parse_args()
     if args.Use_debugger:
         start_debugging()
