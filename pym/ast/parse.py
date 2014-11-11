2f6550b1343047e9020fb8db7830deb26bce0921
J Alan Brogan <github@al-got-rhythm.net>
2014-11-11 09:40:55 +0000

    parse text from a path

--- pym/ast/parse.py
+++ pym/ast/parse.py
@@ -7,3 +7,8 @@ import ast
 def parse(source, path=None):
     path = path if path else '<unknown>'
     return ast.parse(source, path)
+
+
+def parse_path(path):
+    with open(path) as stream:
+        return parse(stream.read(), path)
