417f6740645585890c8500d0ed43152a2f397431
J Alan Brogan <github@al-got-rhythm.net>
2015-12-10 16:41:37 +0000

    Fix NameErrors
    If coder forgot to import it, and pym can find it, then be nice

---
+++ pym/ast/suppress_import_errors.py
@@ -0,0 +1,73 @@
+"""
+from contextlib import contextmanager
+
+@contextmanager
+def tag(name):
+    print("<%s>" % name)
+    yield
+    print("</%s>" % name)
+
+>>> with tag("h1"):
+...    print("foo")
+    <h1>
+    foo
+    </h1>
+"""
+
+def make_import_name_errors(method):
+        import os
+        importable = {'os': os}
+        def import_name_errors(*args, *kwargs):
+                try:
+                        method(*args, **kwargs)
+                except NameError as e:
+                        name = name_error(e)
+                        if not name:
+                                return False
+                        if name in sys.modules:
+                                return False
+                        from importlib import __import__
+                        module = __import__(name)
+                        globals()[name] = module
+                        method(*args, **kwargs)
+                        if name not in importable:
+                                importable[name] = module
+        return import_name_errors
+
+with make_import_name_errors(method) as runner:
+        runner("some", args="here")
+
+class NameErrorHandler(object):
+        def __enter__(self):
+                return self
+        def __exit__(self,exc_type, exc_val, exc_tb):
+                """Exit the context, returning truthiness to suppress exception.
+
+                If an exception occurred while executing the body of the with statement,
+                        the arguments contain the exception type, value and traceback information.
+                        Otherwise, all three arguments are None.
+
+                Returning a true value from this method will cause the with statement to suppress the exception and continue execution with the statement immediately following the with statement. Otherwise the exception continues propagating after this method has finished executing. Exceptions that occur during execution of this method will replace any exception that occurred in the body of the with statement.
+
+                The exception passed in should never be reraised explicitly - instead, this method should return a false value to indicate that the method completed successfully and does not want to suppress the raised exception. This allows context management code to easily detect whether or not an __exit__() method has actually failed.
+                """
+
+def
+        try: method()
+        except NameError as e:
+                importable = ['os']
+                name = name_error(e)
+                if name and name in importable:
+                        from importlib import __import__
+                        __import__(name)
+                        method()
+
+def name_error(exception):
+        string = str(exception)
+    words = string.split(' ')
+    if words[0] != 'name':
+        return None
+    if "'" == words[1][-1] == words[1][0]:
+        name = words[1][1:-1]
+        return name
+    return None
