1326fc3f66017e4e4425572ab2421aa1a548e4a2
J Alan Brogan <github@al-got-rhythm.net>
2016-01-31 12:34:31 +0000

    Better naming

--- pym/reply/main.py
+++ pym/reply/main.py
@@ -3,7 +3,7 @@
 import sys
 
 
-from pym.reply import repl
+from pym.reply import reply
 
 
-sys.exit(repl.main(__file__)) if __name__ == '__main__' else repl.embedder()
+sys.exit(reply.main(__file__)) if __name__ == '__main__' else reply.embedder()
