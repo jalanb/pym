e663390595fb7ee0e6af6e9dc78daa14a3e80c8e
J Alan Brogan <github@al-got-rhythm.net>
2016-10-16 05:30:59 +0100

    Simple is better than complicated
    Rely on dotsite to provide language name
    Only normalise once

--- pym/ast/nst.py
+++ pym/ast/nst.py
@@ -16,56 +16,33 @@ class DirectoryDiskTree(SyntaxTree, site.paths.DirectoryPath):
     pass
 
 
-class NotMyType(ValueError):
-    pass
-
-class LanguageFile(SyntaxTree, site.paths.ExtendedFilePath):
+class LanguageFile(SyntaxTree, site.paths.FilePath):
     """File (leaf of a disk tree, root of contents)"""
-    def __init__(self, s, exts=None):
-        super.__init__(site.paths.FilePath, s, exts)
-        self._exts = exts if exts else []
-
-    def extended(self, test):
-        for ext in self._exts:
-            if self._extended(ext):
-                return True
-            return False
-        raise NotMyType(ext)
-
-    def _extended(self, test):
-        return self.ext() == test
-
-    def _known_ext(self, ext):
-        """Files without extensions can be scripts"""
-        if not ext:
-            return self.lines()[0][:2] == '#!'
-        return ext in self._exts
+    def __init__(self, path, exts=None):
+        super(LanguageFile, self).__init__(path, exts)
+        self.choose_language(exts)
 
 
-
+#  pylint: disable=too-many-ancestors
 class ScriptFile(LanguageFile):
     """Script files have text in a known language
 
-    We expect them to have an extension showing their language
-    Or be a shebang file
+    So far we know Python, Bash, JavaScript, English
     """
-    def __init__(self, s):
-        super(ScriptFile, self).__init__(self, s)
-        self._shebanged = False
+    def _parse(self):
+        self.TODO = True  # raise NotImplementedError
 
-    def _known_ext(self, ext):
-        return super(ScriptFile, self)._known_ext(ext) if ext else self._shebang()
-
-    def _shebang(self):
-        if not self._shebang:
-            self._shebang = self.shebang()
-        self._shebanged = bool(self._shebang)
-        return self._shebanged
+    def __init__(self, path, exts=None):
+        super(ScriptFile, self).__init__(self, path, exts=['.py', '.sh', '.js', '.txt'])
 
 
 class LanguageSyntaxTree(object):
-    def _parse(self, string):
-        raise NotImplementedError
+    pass
+
+
+class EnglishSyntaxTree(LanguageSyntaxTree):
+    """An English syntax tree"""
+    pass
 
 
 class BashSyntaxTree(LanguageSyntaxTree):
@@ -73,7 +50,7 @@ class BashSyntaxTree(LanguageSyntaxTree):
     pass
 
 
-class JavascriptSyntaxTree(LanguageSyntaxTree):
+class JavaScriptSyntaxTree(LanguageSyntaxTree):
     """A JavaScript syntax tree"""
     pass
 
@@ -81,68 +58,73 @@ class JavascriptSyntaxTree(LanguageSyntaxTree):
 class PythonSyntaxTree(LanguageSyntaxTree):
     """A Python syntax tree"""
     def _parse(self):
-        from parse import parse
-        return parse(self))
+        from pym.ast.parse import parse
+        return parse(self)
 
 
 class NormalSyntaxTree(LanguageSyntaxTree):
-    """A normal syntax tree"""
-    def normalize(self):
-        tree = self._parse(self)
-        return self._normal(tree)
+    """A normalised syntax tree"""
+    @property
+    def syntax_tree(self):
+        try:
+            return self._syntax_tree = self._parse(self)
+        except AttributeError:
+            self._syntax_tree = self._parse(self)
+            return self._syntax_tree
 
-    def _normal(self, tree):
-        raise NotImplementedError
+    def tree(self):
+        self._n_tree = self._normalise(self.syntax_tree)
+        return self._n_tree
+
+    def _normalise(self, node):
+        """Convert the syntax tree into a (whatever counts as) Normal (round here) ST"""
+        tree = LazyDefaultDict
+        tree[node]
+        for node in syntax_tree:
+            key = node
+        self.TODO = True  # raise NotImplementedError
+
+
+class EnglishNormalSyntaxTree(NormalSyntaxTree, EnglishSyntaxTree):
+    """A Normalised English syntax tree"""
+    pass
 
 
 class BashNormalSyntaxTree(NormalSyntaxTree, BashSyntaxTree):
     """A Normalised Bash syntax tree"""
-    def _normal(self, ast):
-        """Convert the Bashic AST into a (whatever counts as) Normal (round here) ST"""
-        raise NotImplementedError
+    pass
 
 
 class JavaScriptNormalSyntaxTree(NormalSyntaxTree, JavaScriptSyntaxTree):
     """A Normalised JavaScript syntax tree"""
-    def _normal(self, ast):
-        """Convert the JavaScriptic AST to an NST"""
-        raise NotImplementedError
+    pass
 
 
 class PythonNormalSyntaxTree(NormalSyntaxTree, PythonSyntaxTree):
     """A Normalised Python syntax tree"""
-    def _normal(self, ast):
-        """Convert the Pythonic AST to an NST"""
-        raise NotImplementedError
+    pass
+
+
+class EnglishScript(ScriptFile, EnglishNormalSyntaxTree):
+    """Syntax tree in a English File"""
+    def __init__(self):
+        super(EnglishScript, self).__init__(self, exts=['.txt'])
+
 
 
 class BashScript(ScriptFile, BashNormalSyntaxTree):
     """Syntax tree in a Bash File"""
     def __init__(self):
-        super(ScriptFile, self).__init__(self, exts=['.sh', '.bash'])
-        super(BashNormalSyntaxTree, self).__init__(self.text())
+        super(BashScript, self).__init__(self, exts=['.sh', '.bash'])
 
-    def _shebang(self):
-        result = super._shebang(self)
-        return self._shebang.endswith('bash') if result else False
 
-class JavascriptScript(ScriptFile, JavascriptNormalSyntaxTree):
-    """Syntax tree in a Javascript File"""
+class JavaScriptScript(ScriptFile, JavaScriptNormalSyntaxTree):
+    """Syntax tree in a JavaScript File"""
     def __init__(self):
-        super(ScriptFile, self).__init__(self, exts=['.js'])
-        super(JavascriptNormalSyntaxTree, self).__init__(self.text())
-
-    def _shebang(self):
-        return False
+        super(JavaScriptScript, self).__init__(self, exts=['.js'])
 
 
 class PythonScript(ScriptFile, PythonNormalSyntaxTree):
     """Syntax tree in a Python File"""
     def __init__(self):
-        super(ScriptFile, self).__init__(self, exts=['.py'])
-        super(PythonNormalSyntaxTree, self).__init__(self.text())
-
-    def _shebang(self):
-        result = super._shebang(self)
-        return self._shebang.endswith('python') if result else False
-
+        super(PythonScript, self).__init__(self, exts=['.py'])
