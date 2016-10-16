ce969e45783eb3c82d942f81606d704cc5104d0f
J Alan Brogan <github@al-got-rhythm.net>
2016-10-16 05:28:19 +0100

    Better spacing, mostly

--- pym/reply/reply.py
+++ pym/reply/reply.py
@@ -1,8 +1,7 @@
 """A REPL for pym, from ptpython"""
 
-
 import os
-
+import sys
 
 import ptpython
 from pym.reply import argv
@@ -10,37 +9,33 @@ from pym.reply import pym_repl
 
 
 def embedder():
-    repl.embed({'embedder':embedder}, {})
+    embed({'embedder':embedder}, {})
     return os.EX_OK
 
 
-def embed(globals, locals):
+def embed(global_s, local_s):
     ptpython.repl.PythonRepl = pym_repl.PymRepl
-    import pudb
-    pudb.set_trace()
-    ptpython.repl.embed(globals, locals)
+    ptpython.repl.embed(global_s, local_s)
 
 
 def main(args):
-
+    """Provide a REPL for pym"""
     def run(args):
 
         def add_the_current_directory_to_sys_path():
-            import sys
             if sys.path[0] != '':
                 sys.path.insert(0, '')
-
 
         def disjointed_with_script():
             """Whether we are not interactive, but have a script"""
             return args['<arg>'] and not args['--interactive']
-
 
         def run_args():
             import pudb
             pudb.set_trace()
             sys.argv = args['<arg>']
             program = sys.argv[0]
+            import six
             six.exec_(
                 compile(
                     open(program, "rb").read(),
@@ -60,11 +55,12 @@ def main(args):
 
             from ptpython import repl
             repl.enable_deprecation_warnings()
-            repl.embed(vi_mode=args.get('--vi', True),
-                  history_filename=os.path.join(args['config_dir'], 'history'),
-                  configure=configure,
-                  startup_paths=args.get('startup_paths', []),
-                  title=u'Pym REPL (pym)')
+            repl.embed(
+                vi_mode=args.get('--vi', True),
+                history_filename=os.path.join(args['config_dir'], 'history'),
+                configure=configure,
+                startup_paths=args.get('startup_paths', []),
+                title=u'Pym REPL (pym)')
 
 
         add_the_current_directory_to_sys_path()
@@ -75,7 +71,3 @@ def main(args):
 
     args = argv.parse(args)
     run(args)
-
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
