e9e6c5d451ae8244ba24bb211946a51d3d76f11b
J Alan Brogan <github@al-got-rhythm.net>
2016-11-02 23:58:56 +0000

    Move repl script to bin
    Extract as much ptpython as possible to own module
    
    Packagise ptpython

---
+++ pym/reply/ptpython/args.py
@@ -0,0 +1,60 @@
+"""
+pym: Interactive Python editor.
+Usage:
+    pym [ --emacs ]
+             [ --config-dir=<directory> ] [ --interactive=<filename> ]
+             [--] [ <arg>... ]
+    pym -h
+    pym --help
+
+Options:
+    --emacs                      : Use Emacs keybindings instead of Vi bindings.
+    --config-dir=<directory>     : Pass config directory. By default '~/.pym/'.
+    -i, --import=<filename>      : import this (python) file before starting editor
+
+args are dirs or scripts
+Other environment variables:
+PYTHONSTARTUP: file executed on interactive startup (no default)
+"""
+
+
+def parse(args):
+    """Parse the given args
+
+    args are expected to be like sys.argv
+
+    This is adapted from ptpython's arg parsing
+        but we default to vi-mode, not emacs
+    """
+
+    def ptparse(args):
+        """Parse the given args, as ptpython did"""
+
+        import os
+
+        def make_missing_dir(path):
+            if not os.path.isdir(path):
+                os.mkdir(path)
+            return path
+
+        def create_config_directory():
+            config_dir = os.path.expanduser(args['--config-dir'] or '~/.pym/')
+            return make_missing_dir(config_dir)
+
+        def store_interaction(args):
+            import sys
+            if args['--interactive']:
+                args['startup_paths'].append(args['--interactive'])
+                sys.argv = [args['--interactive']] + args['<arg>']
+
+        args['config_dir'] = create_config_directory()
+        python_startup = os.environ.get('PYTHONSTARTUP', False)
+        args['startup_paths'] = [python_startup] if python_startup else []
+        store_interaction()
+        return args
+
+
+    import docopt
+    args = docopt.docopt(__doc__)
+    args['--vi'] = not bool(args['--emacs'])
+    return ptparse(args)
---
+++ pym/reply/ptpython/repl.py
@@ -0,0 +1,32 @@
+"""A REPL for pym, from ptpython"""
+
+
+import os
+
+
+import pudb
+ptpython = ""
+
+
+
+class PymRepl(ptpython.repl.PythonRepl):
+    def __init__(self, *a, **kw):
+        pudb.set_trace()
+        super(PymRepl, self).__init__(*a, **kw)
+
+def embedder():
+    ptpython.repl.embed({'embedder':embedder}, {})
+    return os.EX_OK
+
+
+def _embed(_globals, _locals):
+    ptpython.repl.PythonRepl = PymRepl
+    pudb.set_trace()
+    ptpython.repl.embed(_globals, _locals)
+
+
+def enable_deprecation_warnings():
+    ptpython.repl.enable_deprecation_warnings()
+
+def embed(*args, **kwargs):
+    ptpython.repl.embed(*args, **kwargs)
