25c3a304a0532f69d899429c59e479ebdf89b550
J Alan Brogan <github@al-got-rhythm.net>
2016-11-03 00:26:06 +0000

    Move debugging into test cases

---
+++ pym/ast/test/test_lists.py
@@ -0,0 +1,24 @@
+"""Test AST list handling"""
+
+import unittest
+
+from pym import lists
+
+
+class TestPymLists(unittest.TestCase):
+    # pylint: disable=protected-access
+    def test_lists(self):
+        line_compare = lists.attribute_comparison('line', 44)
+        lines = [lists.Line(i, l) for i, l in enumerate(
+            open(__file__).read().splitlines(), 1)]
+        actual = lists._search(lines, line_compare)
+        expected = None
+        try:
+            self.assertEqual(actual, expected)
+        except:  # pylint: disable=bare-except
+            import pudb
+            pudb.set_trace()
+
+
+if __name__ == '__main__':
+    unittest.main()
