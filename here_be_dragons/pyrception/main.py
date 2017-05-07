8269a6b8a1cf86986c7806a3bbd6a1e9026d38b2
J Alan Brogan <github@al-got-rhythm.net>
2017-05-07 17:05:18 +0100

    Add runner for pyrception

---
+++ here_be_dragons/pyrception/main.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python2
+"""Script to analyze some python scripts to pyrceive what's in 'em"""
+
+
+import os
+import sys
+from pprint import pprint
+
+
+from pyrception import pyrceive
+
+
+def main(args):
+    paths = [a for a in args if os.path.isdir(a)]
+    for path in paths:
+        print path
+        counts = pyrceive.pyrceive_dir(path)
+        pprint(sorted([(v, k) for k, v in counts.items()]))
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
