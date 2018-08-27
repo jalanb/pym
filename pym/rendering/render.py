c8a2924ec6defda1b9c5c653440fed7d3f7ed304
J Alan Brogan <github@al-got-rhythm.net>
2018-08-27 04:17:56 +0100

    Dispatch render() to different implmentation by type

--- pym/rendering/render.py
+++ pym/rendering/render.py
@@ -1,5 +1,9 @@
 """module to render python asts to text"""
 
+from ast import AST
+from pathlib import Path
+from functools import singledispatch
+
 from .lines import remove_empty_tail
 from .renderer import Renderer
 from ..ast.parse import parse
@@ -7,12 +11,33 @@ from ..ast.transform.commenter import add_comments
 from ..ast.transform.reliner import adjust_lines
 from ..ast.transform.docsourceer import recast_docstrings
 
-def render(node):
+class UnParsable(ValueError):
+    pass
+
+
+@singledispatch
+def render(string: str):
+    node = parse(string) or parse('', string)
     if not node:
-        return None
+        raise UnParsable(string)
+    return render(node)
+
+
+@render.register
+def _(node: AST):
+    if not node:
+        return ''
     renderer = Renderer()
     renderer.visit(node)
     return '\n'.join(remove_empty_tail(renderer.lines))
+
+
+@render.register
+def _(path: Path):
+    if not path.exists():
+        raise UnParsable(str(path))
+    node = parse('', path)
+    return render(node)
 
 
 def re_render(source, path=None):
