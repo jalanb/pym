c0feec0f0ed3df255855448dadc1fc519a25703a
J Alan Brogan <github@al-got-rhythm.net>
2018-10-24 00:12:49 +0100

    Add a Liner

--- pym/ast/visit/visitors.py
+++ pym/ast/visit/visitors.py
@@ -21,3 +21,18 @@ class Sourcer(PymVisitor):
     def generic_visit(self, node):
         line_number = node.lineno
         line = linecache.getline(filename, line_number).rstrip()
+
+
+class Liner(PymVisitor):
+    def __init__(self):
+        super(Liner, self).__init__()
+        self._old = None
+        self.lines = {}
+
+    def generic_visit(self, node):
+        number = node.lineno
+        try:
+            first = self.lines[number][0]
+            self.lines[number] = (first, node)
+        except KeyError:
+            self.lines[number] = (node, node)
